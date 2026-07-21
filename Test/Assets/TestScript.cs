6f3c2ff027ebc9f34eb9229de7f9170d46578977
annalenaspring <anna.spring.iseli@gmail.com>
2026-07-21 19:51:03 +0200

    hallo auch aus der ferne

--- Test/Assets/TestScript.cs
+++ Test/Assets/TestScript.cs
@@ -2,16 +2,15 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update
     void Start()
     {
-        
+        Debug.Log("Hallo! Auch aus der Ferne!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    Debug.Log("Hallo aus der Ferne!");
 }
